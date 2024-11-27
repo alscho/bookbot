9b21e9cafb1909527721a0ecc92a2a0d6befa623
alscho <alexander2791@hotmail.com>
2024-11-27 13:21:34 +0100

    updated report

--- main.py
+++ main.py
@@ -13,15 +13,29 @@ def test(book_path, num_words, chars_dict):
     except Exception as e:
         print(e)
 
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dict(char_dict):
+    char_stats = []
+    for k in char_dict:
+        if k.isalpha():
+            char_stats.append({"name": f"{k}", "num": char_dict[k]})
+    
+    char_stats.sort(reverse=True, key=sort_on)
+
+    return char_stats
+
 
 def print_report(num_words, char_dict, filepath):
-    alphabet = "abcdefghiklmnopqrstuvwxyz"
+
+    sorted = sort_dict(char_dict)
 
     print(f"--- Begin report of {filepath} ---")
     print(f"{num_words} words found in the document")
 
-    for letter in alphabet:
-        print(f"The '{letter}' character was found {char_dict[letter]} times")
+    for place in sorted:
+        print(f"The '{place["name"]}' character was found {place["num"]} times")
     
     print(f"--- End report ---")
 
