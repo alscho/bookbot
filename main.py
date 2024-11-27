b780ef27d6731eb95c7cd8a0f4c27ae2c3fbd657
alscho <alexander2791@hotmail.com>
2024-11-27 12:55:03 +0100

    added report printer

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_char_count(text)    
-    print(chars_dict)
+    
+    test(book_path, num_words, chars_dict)
 
 
-def test(string):
+def test(book_path, num_words, chars_dict):
     try:
-        print(get_char_count(string))
+        print_report(num_words, chars_dict, book_path)
     except Exception as e:
         print(e)
+
+
+def print_report(num_words, char_dict, filepath):
+    alphabet = "abcdefghiklmnopqrstuvwxyz"
+
+    print(f"--- Begin report of {filepath} ---")
+    print(f"{num_words} words found in the document")
+
+    for letter in alphabet:
+        print(f"The '{letter}' character was found {char_dict[letter]} times")
+    
+    print(f"--- End report ---")
 
 
 def get_char_count(text):
